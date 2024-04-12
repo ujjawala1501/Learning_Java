1b5aab80eb979f10f229729b44740825761b1809
Ujjawala Katakwar <ujjawala1501@gmail.com>
2024-04-12 19:59:44 +0530

    First java Pgm

---
+++ hello.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+class Hello
+{
+public static void main(String a[])
+{
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    int c = scanner.nextInt();
+    System.out.println("hello java again");
+}
+}
