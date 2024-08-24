64cca2fc6599786e1cf81d01db49caf078527981
[Ujjawala Katakwar] <ujjawala1501@gmail.com>
2024-08-24 18:36:56 +0530

    Method notes added

--- Method.java
+++ Method.java
@@ -1,4 +1,5 @@
-class Calculator{
+ class Calculator{
+    int num = 5;   //This is an instance variable this can also be accessed with the help of the object of class we will create in main method.
     public int add(int n1,int n2){
         int result = n1 + n2 ;
         System.out.println("Currently inside Calculator class and add method");
@@ -10,6 +11,10 @@ public class Method
 {
     public static void main(String[] args){
         Calculator cal = new Calculator();
+        Calculator cal1 = new Calculator(); //created 2nd object of class Calculator
+        cal1.num = 8; //example that the instance can be changed through object created for class.
+        System.out.println(cal.num); //this gives num=5
+        System.out.println(cal1.num); //this give num=8
         int n1 = 2;
         int n2 = 3;
         System.out.println("Inside main method of Method class and in the next line calling add method");
