505fe1502af77e966c7b5a4689f7a1cd8b61ee54
[Ujjawala Katakwar] <ujjawala1501@gmail.com>
2024-08-24 18:44:21 +0530

    Added notes to Array

--- Array2.java
+++ Array2.java
@@ -2,13 +2,15 @@ import java.util.*;
 public class Array2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of array:");
         int size =sc.nextInt();
+        System.out.println("Enter the number as many times as you've given size of the array:");
         int numbers[] =new int[size];
-        //for loop for input
+        //for loop to take input
         for(int i=0;i<size;i++){
-            numbers[i] =sc.nextInt();
+            numbers[i] = sc.nextInt();
         }
-        //for loop for output of array
+        //for loop to give output of array
         for(int i=0; i<size;i++){
             System.out.println(numbers[i]);
         }
