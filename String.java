628209e8479121fd9370eed232794ecc78f94565
[Ujjawala Katakwar] <ujjawala1501@gmail.com>
2024-08-30 22:36:28 +0530

    String

--- String.java
+++
@@ -1,2 +0,0 @@
-//String is not primitive type like other data types they start
-// with small letter but while describing "String" it starts with capital "S"its a class .
---
+++ String_var.java
@@ -0,0 +1,38 @@
+//String is not primitive type like other data types they start
+// with small letter but while describing "String" it starts with capital "S"its a class .
+
+import java.lang.String;
+public class String_var{
+    public static void main(String[] args){
+        String Name = new String("Neer");   //We can assign a value like this.
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  //We can assign a value like this as well.
+        System.out.println("The length of the txt string is: " + txt.length());
+
+        //Below is example of how String is a class and has Methods one of which is "indexof()"
+        String txt2 = "Please locate where 'locate' occurs!";
+        System.out.println(txt.indexOf("locate")); // Outputs 7
+
+        //Concatenation
+        String firstName = "John";
+        String lastName = "Doe";
+        System.out.println(firstName + " " + lastName);
+
+        //Concatenation using "concat()" method
+        String firstName2 = "John ";
+        String lastName2 = "Doe";
+        System.out.println(firstName.concat(lastName));
+
+        //Java uses the + operator for both addition and concatenation.
+        //Numbers are added. Strings are concatenated.
+        //Below is the example of how int type works and how string type works
+        int x = 10;
+        int y = 20;
+        int z = x + y;  // z will be 30 (an integer/number)
+
+
+        String xA = "10";
+        String yA = "20";
+        String zA = x + y;  // z will be 1020 (a String)
+    }
+
+}
