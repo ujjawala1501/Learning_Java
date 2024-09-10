1b78af48a6b13fc43af75fd867c7c226b3274ddd
ujjawala1501 <ujjawala1501@gmail.com>
2024-09-10 22:36:14 +0530

    added constructor notes

--- Main.java
+++ Main.java
@@ -14,3 +14,9 @@ public class Main {
   }
   
   // Outputs 5
+  /*the constructor name must match the class name, and it cannot have a return type (like void).
+
+the constructor is called when the object is created.
+
+All classes have constructors by default: if you do not create a class constructor yourself, Java creates one for you. 
+However, then you are not able to set initial values for object attributes. */
