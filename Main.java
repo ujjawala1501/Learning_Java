1104fa59a5a8fe657a744a17248c0ffd776bdf95
ujjawala1501 <ujjawala1501@gmail.com>
2024-09-12 22:15:36 +0530

    notes added

--- Main.java
+++ Main.java
@@ -1,12 +1,24 @@
 // Create a Main class
-public class Main {
+
+  /*the constructor name must match the class name, and it cannot have a return type (like void).
+
+the constructor is called when the object is created.
+
+All classes have constructors by default: if you do not create a class constructor yourself, Java creates one for you. 
+However, then you are not able to set initial values for object attributes. */
+class Main {
     int x;  // Create a class attribute
-  
+    int age;
+    String name;
     // Create a class constructor for the Main class
     public Main() {
       x = 5;  // Set the initial value for the class attribute x
+
     }
-  
+    public Main(int a,String n){ //Parameterized Constructor
+      this.age = a;
+      this.name = n;
+    }
     public static void constructor(String[] args) {
       Main myObj = new Main(); // Create an object of class Main (This will call the constructor)
       System.out.println(myObj.x); // Print the value of x
@@ -14,9 +26,3 @@ public class Main {
   }
   
   // Outputs 5
-  /*the constructor name must match the class name, and it cannot have a return type (like void).
-
-the constructor is called when the object is created.
-
-All classes have constructors by default: if you do not create a class constructor yourself, Java creates one for you. 
-However, then you are not able to set initial values for object attributes. */
