61481e730401ebf17366e9d57247329174c65e31
ujjawala1501 <ujjawala1501@gmail.com>
2024-09-18 16:57:04 +0530

    inheritance

--- Inheritance/calc2.java
+++ Inheritance/calc2.java
@@ -1,7 +1,7 @@
 //repeating same code is redundancy
 package Inheritance;
 
-public class calc2 {
+public class calc2 extends calc1{
     public int multiply(int n1 , int n2){
         return n1*n2;
     }
