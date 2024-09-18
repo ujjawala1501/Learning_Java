999eab73527972aaf5f85fc456df00f79cbec002
ujjawala1501 <ujjawala1501@gmail.com>
2024-09-18 15:52:00 +0530

    Inheritance from telusko

---
+++ Inheritance/calc2.java
@@ -0,0 +1,13 @@
+//repeating same code is redundancy
+package Inheritance;
+
+public class calc2 {
+    public int multiply(int n1 , int n2){
+        return n1*n2;
+    }
+
+    public int division(int n1 , int n2){
+        return n1/n2;
+    }
+    
+}
