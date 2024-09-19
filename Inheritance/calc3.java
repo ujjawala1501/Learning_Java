1fdb2060e1840c88047d9a3c5994fa24fe26d6e7
ujjawala1501 <ujjawala1501@gmail.com>
2024-09-19 22:59:06 +0530

    multi inherit

---
+++ Inheritance/calc3.java
@@ -0,0 +1,7 @@
+package Inheritance;
+
+public class calc3 extends calc2{
+    public double power(int n1,int n2){
+        return Math.pow(n1,n2);
+    }
+}
--- Inheritance/maincalc.java
+++ Inheritance/maincalc.java
@@ -1,15 +1,17 @@
 //When there is inheritance each class if in different file must be compiled and better if compiled together like below
 // "Javac Inheritance/calc1.java Inheritance/calc2.java Inheritance/maincalc.java "
+//Single level inheritance is when one class extends one class,multi level is when 1<2<3 .like below example.
+//A child cannot extend two parent at a time.
 package Inheritance;
 
 public class maincalc {
     public static void main(String[] args){
-        calc2 obj = new calc2();
+        calc3 obj = new calc3();
         int add_rslt = obj.add(2,3);
         int sub_rslt = obj.sub(5,2);
         int multiply_rslt = obj.multiply(2,3);
         int division_rslt = obj.division(2,3);
-
+        double power_rslt = obj.power(2,3);
     //System.out.println("Addition : " +add_rslt+ "sub" +sub_rslt+ "multiply" +multiply_rslt+ "division" +division_rslt);
     System.out.println("Addition: " + add_rslt + ", Subtraction: " + sub_rslt + ", Multiplication: " + multiply_rslt + ", Division: " + division_rslt);
     }
