4d3610a2ee29526e92e1b27ac1106f516b50f34a
ujjawala1501 <ujjawala1501@gmail.com>
2024-09-19 23:35:01 +0530

    package

---
+++ Package/file1.java
@@ -0,0 +1,11 @@
+package Package; //This is how it should be mentioned in all the files
+//if a file is outside of package like other files here which are not part of "package" folder you have to mention "import foldername.class"
+//example: 
+import Inheritance.calc1 ;
+//import java.util.ArrayList // java is a folder,util is a folder ,Arraylist is a class.
+//all inbuild are in a package
+//System is a class 
+//Java.lang.System is by default imported hence System is not required to be imported.
+public class file1 {
+    
+}
