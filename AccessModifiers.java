27141b8874cfd510f54e7c69b327a280cc33057d
ujjawala1501 <ujjawala1501@gmail.com>
2024-09-20 22:54:26 +0530

    default modifier

--- AccessModifiers.java
+++ AccessModifiers.java
@@ -1,9 +1,9 @@
 public class AccessModifiers {
     
 }
-Public
-Description: Members declared as public are accessible from any other class in any package.
-java
+//Public
+//Description: Members declared as public are accessible from any other class in any package.
+//java
 public class PublicExample {
     public int publicVar = 10;
 
@@ -12,9 +12,9 @@ public class PublicExample {
     }
 }
 
-2. Private
-Description: Members declared as private are accessible only within the class they are declared in. They cannot be accessed from outside the class.
-java
+//2. Private
+//Description: Members declared as private are accessible only within the class they are declared in. They cannot be accessed from outside the class.
+//java
 public class PrivateExample {
     private int privateVar = 20;
 
@@ -27,9 +27,9 @@ public class PrivateExample {
     }
 }
 
-3. Protected
-Description: Members declared as protected are accessible within the same package and by subclasses (even if they are in different packages).
-java
+//3. Protected
+//Description: Members declared as protected are accessible within the same package and by subclasses (even if they are in different packages).
+//java
 public class ProtectedExample {
     protected int protectedVar = 30;
 
@@ -38,20 +38,14 @@ public class ProtectedExample {
     }
 }
 
-4. Default (Package-Private)
-Description: If no access modifier is specified, it is considered default. Members with default access are accessible only within classes in the same package.
-java
+//4. Default (Package-Private)
+//Description: If no access modifier is specified, it is considered default. Members with default access are accessible only within classes in the same package.
+//java
 class DefaultExample { // No 'public' modifier
-    int defaultVar = 40; // Default access
+int defaultVar = 40; // Default access
 
-    void displayDefault() {
-        System.out.println("Default Variable: " + defaultVar);
-    }
+void displayDefault() {
+    System.out.println("Default Variable: " + defaultVar);
+}
 }
 
-Summary of Differences
-Modifier	Class Level	Package Level	Subclass Level	Global Level
-public	    Yes	        Yes	            Yes	            Yes
-protected	Yes	        Yes	            Yes	            No
-private	    Yes	        No	            No	            No
-Default	    Yes	        Yes	            No	            No
