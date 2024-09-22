1122bcccd39b867c1775a2d5da7cec81bd222b6f
ujjawala1501 <ujjawala1501@gmail.com>
2024-09-22 22:38:00 +0530

    added notes for polymorph

--- PolymorphismTelusko.java
+++ PolymorphismTelusko.java
@@ -1,4 +1,21 @@
 //poly - many ,morphism -behaviour (based on situation)
+//1)compile time(overloading)   2)runtime (overriding)
 public class PolymorphismTelusko {
     
 }
+//dynamic method dispatch (for runtime polymorphism)
+/* class A {
+    public void show(){
+    }
+}
+
+class B extends A{
+}
+
+class main{
+    public static void main(String[] args){
+        A obj = new B(); //type is parent(A) but object is child(B)
+        obj.show();
+    }
+}
+*/
