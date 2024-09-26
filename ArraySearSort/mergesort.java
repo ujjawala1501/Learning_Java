d93a9920bd82e66456e3a479d3e79aa9a0e3c1a9
ujjawala1501 <ujjawala1501@gmail.com>
2024-09-26 23:13:43 +0530

    merge sort

--- ArraySearSort/mergesort.java
+++ ArraySearSort/mergesort.java
@@ -8,12 +8,29 @@ package ArraySearSort;
 
 public class mergesort {
 
+    public static void mergesort(int arr[], int l , int r ){  //l=left , r= right
+        if(l<r){
+            int mid = (l+r)/2;
+            mergesort(arr,l,mid);
+            mergesort(arr,mid+1,r);
+            
+            merge(arr ,l,mid ,r);
+        }
+
+    }
+    public static void merge(int[] arr,int l ,int mid,int r){
+        int lArr[]=new int[];
+        int rArr[]=new int[];
+    }
     public static void main(String[] args){
         int arr[] = {3,5,1,4,6,2};
         for(int n:arr){
             System.out.print(n + " ");
         }
             System.out.println();
+
+            mergesort(arr , 0,arr.length-1);
+
             System.out.println("after sorting");
             for(int n :arr){
                 System.out.println(n + " ");
