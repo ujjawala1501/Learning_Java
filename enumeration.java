60260a46ab63325c4c7a37c9f459aaae06ce2610
ujjawala1501 <ujjawala1501@gmail.com>
2024-09-30 16:43:41 +0530

    enumeration

--- enumeration.java
+++ enumeration.java
@@ -17,3 +17,30 @@ public class enumeration {
     }
     
 }
+//enumeration with if.else and switch 
+/* 
+enum Status1{
+    Running, Failed, Pending, Success;
+}
+public class enumeration{
+    public static void main(String[] args){
+        Status1 s2 = Status1.Pending; //setting "s" value as Pending.
+
+        if(s2==Status1.Running){
+            System.out.println("all good running");
+        }
+        else if(s2==Status1.Failed){
+            System.out.println("it failed ");
+        }
+        else if(s2==Status1.Pending){
+            System.out.println("pending wait");
+        }
+        else{
+            System.out.println("its Success");
+        }
+
+    }
+}
+*/
+ 
+ 
