81476f01263c19a1a554ab8c1950414ca093a1e4
ujjawala1501 <ujjawala1501@gmail.com>
2024-10-02 21:54:19 +0530

    annotation

--- Annotation.java
+++ Annotation.java
@@ -1,12 +1,14 @@
-//annotation help to communicate with compiler with the problem and it helps like you can mention in B that its override and it will let you know if you c=have created any methods with same name which can be overridden.
+//annotation help to communicate with compiler with the problem and it helps like you can mention in B that its override and it will let you know if you c=have created any methods with same name which can be overridden(if name is biiiigg and you are unable to check yourself of course)
+//there are other annotation inbuilt as well.
 class A 
 {
-    public void show(){
+    public void showThedatawhichbelongstothisclass(){
         System.out.println("In A show");
     }
 }
 class B extends A{
-    public void show(){
+    @Override
+    public void showThedatawhichbelongstothisclass(){
         System.out.println("In B show");
     }
 
