6eccc116d26714754ffb3f2465e91d13808af5f2
ujjawala1501 <ujjawala1501@gmail.com>
2024-10-03 21:46:09 +0530

    annotation

--- Annotation.java
+++ Annotation.java
@@ -21,3 +21,15 @@ public class Annotation
         obj.show();
     }
 }
+/*
+ Annotations in Java are a form of metadata that provide additional information about the program. They are used to give instructions to the compiler or to be processed at runtime by various tools and frameworks. Annotations start with the @ symbol and can be applied to classes, methods, fields, parameters, and other program elements.
+Key Features of Annotations
+Metadata:
+Annotations act as metadata, meaning they describe other data. They do not change the behavior of the program directly but can influence how the program is compiled or executed.
+Syntax:
+The basic syntax of an annotation is:
+java
+@AnnotationName
+
+For example, @Override is an annotation that indicates a method is overriding a method in its superclass.
+ */
