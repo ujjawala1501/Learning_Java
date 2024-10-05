b24fefe2783560c9abbb5bde722631666ed76dbb
ujjawala1501 <ujjawala1501@gmail.com>
2024-10-05 22:07:21 +0530

    lambda

--- Lambdaexpression.java
+++ Lambdaexpression.java
@@ -7,13 +7,14 @@ interface A
 
 public class Lambdaexpression 
 {
+    //  Below anything that is commented is normalyy how it works but hence we using lambda (arrow) we do not need those things, so commented them.
     public static void main(String[] args){
-        A obj = new A();
-        {
-            public void show(){
+        A obj = ()-->///new A();/Adding this Arrow here is the only thing that is lambda expression 
+     //   {
+            //public void show(){ //commented because usign lambda (arrow thing...)
                 System.out.println("in show method");
-            }
-        };
+            //}
+     //    };
         obj.show();
     }
     
