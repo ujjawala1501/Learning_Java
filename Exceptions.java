7d1a888ef8dd8a8e8bcf3bb4dc86cd576d160814
ujjawala1501 <ujjawala1501@gmail.com>
2024-10-05 22:57:11 +0530

    Exception handling

--- Exceptions.java
+++ Exceptions.java
@@ -2,10 +2,13 @@
  Types of Errors:
  1)compile time error > we are shown at compiling time.
 
- 2) Runtime Error > code is working smoothly ,no compile time error but suddenly the code STOPSS!!  (this is where exception handling comes which means to handle the pre assumed errors)
+ 2) Runtime Error > code is working smoothly ,no compile time error but suddenly the code STOPSS!!  (this is where exception handling comes which means to handle the pre assumed errors)(example . > arrayoutofbound ,arithmeticexception etc)
 
  3) Logical error > when output is wrong(you fucked up!)now you need to test everything EVERY DAMN THING !
+ 
+ We can have multiple try ,catch executes only when there is some error otherwise it will pass.
  */
+//Some exceptions which we are not sure of ->catch(Exception e) //Exception is a parent class it can handle any exception .
 public class Exceptions {
     public static void main(String[] args){
         int i = 9;
@@ -14,9 +17,20 @@ public class Exceptions {
         {
          j = 18/i; //this is critical ,what if value of i is 0?(youll get arithmetic exception)and this will stop the execution
         }
+        catch(ArrayIndexOutOfBoundsException e)//e can be obj as well)
+        {
+            ///if trying to handle multiple try in single catch, you can either give if statement inside cath OR you can write multiple cath OR mention the exception with catch like this ->catch(ArithmeticException e)
+            System.out.println("Something went wrong"+ e);//"e" here will show the exception we got.
+        }
+        catch(ArithmeticException e)//e can be obj as well)
+        {
+            ///if trying to handle multiple try in single catch, you can either give if statement inside cath OR you can write multiple cath OR mention the exception with catch like this ->catch(ArithmeticException e)
+            System.out.println("Something went wrong"+ e);//"e" here will show the exception we got.
+        }
         catch(Exception e)//e can be obj as well)
         {
-            System.out.println("Something went wrong");
+            ///if trying to handle multiple try in single catch, you can either give if statement inside cath OR you can write multiple cath OR mention the exception with catch like this ->catch(ArithmeticException e)
+            System.out.println("Something went wrong"+ e);//"e" here will show the exception we got.
         }
         System.out.println(j);
     }
