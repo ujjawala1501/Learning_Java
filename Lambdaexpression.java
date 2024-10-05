283b9f46c172b7887f229ed15a921d7ff1aa44b6
ujjawala1501 <ujjawala1501@gmail.com>
2024-10-05 22:13:57 +0530

    lambda works only with functional interface

--- Lambdaexpression.java
+++ Lambdaexpression.java
@@ -1,7 +1,7 @@
 @FunctionalInterface
 interface A 
 {
-    void show();
+    void show(int i,int j);
 
 }
 
@@ -9,13 +9,13 @@ public class Lambdaexpression
 {
     //  Below anything that is commented is normalyy how it works but hence we using lambda (arrow) we do not need those things, so commented them.
     public static void main(String[] args){
-        A obj = ()-->///new A();/Adding this Arrow here is the only thing that is lambda expression 
+        A obj = (int i , int j)-->///new A();/Adding this Arrow here is the only thing that is lambda expression 
      //   {
             //public void show(){ //commented because usign lambda (arrow thing...)
                 System.out.println("in show method");
             //}
      //    };
-        obj.show();
+        obj.show(5,6);
     }
     
 }
