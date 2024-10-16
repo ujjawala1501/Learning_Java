c27bd52da0eccc3cb125f64cf2eb8cd8f22879bb
ujjawala1501 <ujjawala1501@gmail.com>
2024-10-16 16:15:51 +0530

    Threads

--- Threads.java
+++ Threads.java
@@ -1,4 +1,12 @@
 //threads are smallest unit we can work 
+/*
+ Roles of run() and start() in Java Threads
+In Java, the run() and start() methods are fundamental to thread management and execution. Here's a concise overview of their roles:
+start() Method:
+Purpose: The start() method is used to initiate a new thread. When called, it allocates a new stack for the thread and invokes the run() method in a separate call stack.
+Execution: It allows the thread to enter the Runnable state, where it can be scheduled for execution by the thread scheduler.
+Concurrency: Multiple threads can run concurrently when start() is called on different thread instances.
+ */
 class A extends Thread{
     public void run()
     {
