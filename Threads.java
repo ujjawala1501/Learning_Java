334a1ae80492dedadc41808a7c2fe609fac9a634
ujjawala1501 <ujjawala1501@gmail.com>
2024-10-16 16:15:05 +0530

    Threads

--- Threads.java
+++ Threads.java
@@ -1,4 +1,26 @@
 //threads are smallest unit we can work 
+class A extends Thread{
+    public void run()
+    {
+        for(int i =0;i<100;i++){
+            System.out.println("hi");
+        }
+    }
+
+}
+class B extends Thread {
+    public void run(){
+        for(int i=0;i<100;i++){
+            System.out.println("Hello");
+        }
+    }
+}
 public class Threads {
-    
+    public static void main(String[] args){
+        A obj1 = new A();
+        B obj2 = new B();
+
+        obj1.start();
+        obj2.start();
+    }
 }
