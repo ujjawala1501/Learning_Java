abf858deadedd21236155f13f585e36ed11c2d99
ujjawala1501 <ujjawala1501@gmail.com>
2025-01-17 22:37:12 +0530

    ffirst

---
+++ hello2.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class hello2 {
+    public static void main(String[] args){
+        Scanner ref = new Scanner(System.in);
+        int n = ref.nextInt();
+        for (int i =1;i<=n;i++){
+            for (int j=1;j<=n+1-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+    //     int sum = 1;
+    //     for(int i=1;i<=n;i++){
+    //         for(int j=1;j<=i;j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+        
+    // }}
+        //int arr1[] = new arr[5];
+        // String n = ref.nextLine();
+
+        // switch (n) {
+        //     case "a":
+        //         System.out.println("hi");
+        //         break;
+        //     case "f":
+        //         System.out.println("hello");
+        //         break;
+        //     case "b":
+        //         System.out.println("hi b");
+        //         break;
+        //     case "c":
+        //         System.out.println("hello c");
+        //         break;
+        //     default:
+        //         break;
+  //      }
+//}
+//}
+
